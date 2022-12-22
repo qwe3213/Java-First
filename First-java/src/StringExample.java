7272b6ac8137502dcab59e7d23a2c25d5fc90c29
hayun <hs183000@naver.com>
2022-12-22 22:43:56 +0900

    문자와 문자열

---
+++ First-java/src/StringExample.java
@@ -0,0 +1,11 @@
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("나는 문자열"); //겹따옴표는 문자열
+        System.out.println('난'); //홑따옴표는 문자 -> 한글자밖에 입력이안됨
+
+        System.out.println("안녕 나는 \"자바\"야."); //문자열 내에 따옴표를 표시하기위한 방법
+
+        System.out.println("치킨은 살 안쪄요. \n살은 내가 쪄요");
+    }
+ }
+
