19a21e6676e16c04c32e7bc25394af49f961a904
hayun <hs183000@naver.com>
2022-12-31 00:09:42 +0900

    Boolean 타입 기능

---
+++ First-java/src/BooleanExample.java
@@ -0,0 +1,8 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean trueValue = true;  //논리형 타입
+        boolean falseValue = false;
+        boolean question = 5>3; //5는 3보다 크므로 true를 출력한다.
+        System.out.println(question);
+    }
+}
